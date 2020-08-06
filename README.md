346a3618f485b69f2b20f4dd203f10a55ad5c2d3
Maximiliano Blanco <inkcoming@protonmail.com>
2020-08-06 20:10:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Film Here
+
+A website to share locations availables to shoot a movie, video or take photos.
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/index.css';
+import '@reach/combobox/styles.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
