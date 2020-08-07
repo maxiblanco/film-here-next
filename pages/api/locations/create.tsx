13fd7936e34304fcddc8d43a12e7c9c8bce23a08
Maximiliano Blanco <inkcoming@protonmail.com>
2020-08-06 23:25:26 -0300

    Switched to typescript for api

---
+++ pages/api/locations/create.tsx
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prisma = new PrismaClient({ log: ['query'] });
+
+  try {
+    const { location: locationData } = req.body;
+    const location = await prisma.location.create({
+      data: {
+        latitude: locationData.latitude,
+        longitude: locationData.longitude,
+      },
+    });
+    res.status(201);
+    res.json({ location });
+  } catch (err) {
+    res.status(500);
+    res.json({ error: 'Sorry unable to save location to database' });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
