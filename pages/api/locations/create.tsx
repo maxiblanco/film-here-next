5f57afb65cc0daf3ad8112ad0158c6d3a71d5918
Maximiliano Blanco <inkcoming@protonmail.com>
2020-08-07 16:37:44 -0300

    Create locations API, prisma to postgres

--- pages/api/locations/create.tsx
+++ pages/api/locations/create.tsx
@@ -1,26 +1,23 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function (
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const prisma = new PrismaClient({ log: ['query'] });
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient({ log: ['query'] });
 
-  try {
-    const { location: locationData } = req.body;
-    const location = await prisma.location.create({
-      data: {
-        latitude: locationData.latitude,
-        longitude: locationData.longitude,
-      },
-    });
-    res.status(201);
-    res.json({ location });
-  } catch (err) {
-    res.status(500);
-    res.json({ error: 'Sorry unable to save location to database' });
-  } finally {
-    await prisma.$disconnect();
-  }
+    try {
+        const { location: locationData } = req.body;
+        const location = await prisma.location.create({
+            data: {
+                latitude: locationData.latitude,
+                longitude: locationData.longitude
+            }
+        });
+        res.status(201);
+        res.json({ location });
+    } catch (err) {
+        res.status(500);
+        res.json({ error: 'Sorry unable to save location to database' });
+    } finally {
+        await prisma.$disconnect();
+    }
 }
